d8528ba710697627496429f36a9d8e6bbf01d74b
nato-git <s.f.h.o1219@gmail.com>
2025-11-26 23:37:33 +0900

    入力ボタンの作成

--- app/src/main/java/com/example/characterlistapp/MainActivity.kt
+++ app/src/main/java/com/example/characterlistapp/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.characterlistapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val Createfile: TextView = findViewById<TextView>(R.id.NewCreateFile)
+        Createfile.isVisible = false
+
+        val CreateButton: Button = findViewById<Button>(R.id.Firstbutton)
+        CreateButton.setOnClickListener{
+            Log.d("buttonmsg","This is a Button")
+            Createfile.isVisible = !Createfile.isVisible
         }
     }
 }
