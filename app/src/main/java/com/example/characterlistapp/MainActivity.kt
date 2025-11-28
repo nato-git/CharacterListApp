b48d6434333a2000f8e94c2b15df8e0de8db2fdb
nato-git <s.f.h.o1219@gmail.com>
2025-11-29 00:00:03 +0900

    SQLiteの追加。ただいまクラッシュの危険あり

--- app/src/main/java/com/example/characterlistapp/MainActivity.kt
+++ app/src/main/java/com/example/characterlistapp/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.util.Log
-import android.view.View
+import android.view.Gravity
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.isVisible
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,20 +26,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         val Createfile: TextView = findViewById<TextView>(R.id.NewCreateFile)
+        val Createbutton: Button = findViewById<Button>(R.id.NewCreateFileButton)
         Createfile.isVisible = false    //入力スポット隠す
+        Createbutton.isVisible = false
 
         //入力スポット切り替え
         val CreateMenuButton: Button = findViewById<Button>(R.id.Firstbutton)
         CreateMenuButton.setOnClickListener{
             Log.d("buttonmsg","This is a Button")
             Createfile.isVisible = !Createfile.isVisible
+            Createbutton.isVisible = !Createbutton.isVisible
+            val ClearText: EditText? = findViewById<EditText>(R.id.NewFileName)
+            if(ClearText != null) {
+                ClearText.setText("")
+            }
         }
 
         //ファイル名決定
         val CreateButtom:Button = findViewById<Button>(R.id.NewCreateFileButton)
         CreateButtom.setOnClickListener{
-            val FileNameId: EditText = findViewById<EditText>(R.id.NewFileName)
-            val NewFilename: String = FileNameId.text.toString()
+            val FileNameId: EditText? = findViewById<EditText>(R.id.NewFileName)
+            if(FileNameId != null) {
+                val NewFilename: String = FileNameId.text.toString()
+                SQLiteFile.addList(applicationContext, NewFilename)
+                Create(NewFilename)
+                FileNameId.setText("")
+            }
         }
+    }
+
+    fun Create(text: String){
+        val layout = findViewById<LinearLayout>(R.id.main)
+        layout.gravity = Gravity.CENTER
+        val texting = TextView(this)
+        texting.text = text
+        texting.gravity = Gravity.CENTER
+        texting.textSize = 25F
+        layout.addView(texting)
     }
 }
--- app/src/main/java/com/example/characterlistapp/SQLiteFile.kt
+++ app/src/main/java/com/example/characterlistapp/SQLiteFile.kt
@@ -3,7 +3,30 @@ package com.example.characterlistapp
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.content.ContentValues
 
-class SQLiteFile{
 
+class SQLiteFile(context: Context,databaseName: String, factory: SQLiteDatabase.CursorFactory?, version: Int):
+    SQLiteOpenHelper(context,databaseName,factory,version) {
+    override fun onCreate(database: SQLiteDatabase?) {
+        database?.execSQL("create table if not exists TitleFile(name text)")
+    }
+
+    override fun onUpgrade(database: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        if (oldVersion < newVersion) {
+            database?.execSQL("alter table TitleFile add column deleteFlag INTEGER default 0")
+        }
+    }
+
+    companion object {
+        fun addList(context: Context, CreateName: String) {
+            val dbHelper = SQLiteFile(context, "TitleFile", null, 1)
+            val database = dbHelper.writableDatabase
+            val values = ContentValues().apply {
+                put("name", CreateName)
+            }
+            database.insert("TitleFile", null, values)
+            database.close()
+        }
+    }
 }
